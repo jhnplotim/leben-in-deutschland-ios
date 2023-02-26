44731cb6b9df45a607c74347a742afbb02edb3e0
John Paul Otim <jhnplotim@gmail.com>
2023-02-26 11:58:52 +0100

    feat: project initialisation
    - project initialisation

---
+++ LebenInDeutschland/LebenInDeutschlandApp.swift
@@ -0,0 +1,17 @@
+//
+//  LebenInDeutschlandApp.swift
+//  LebenInDeutschland
+//
+//  Created by John Paul Otim on 26.02.23.
+//
+
+import SwiftUI
+
+@main
+struct LebenInDeutschlandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
