c80ca1c7a9b87cd1501b4fbe6cefba6459537f28
John Paul Otim <johnpaul.otim@muehlbauer.de>
2023-02-28 18:25:54 +0100

    feat: work in progress - Radio button
    - work in progress - Radio button

---
+++ LebenInDeutschland/Views/Helpers/RadioButtons/RadioButton.swift
@@ -0,0 +1,62 @@
+//
+//  RadioButton.swift
+//  LebenInDeutschland
+//
+//  Created by Otim John Paul on 28.02.23.
+//
+
+import SwiftUI
+
+struct RadioButton: View {
+    @Environment(\.colorScheme) var colorScheme
+    
+    let id: String
+    let callback: (String)->()
+    let selectedID : String
+    let size: CGFloat
+    let color: Color
+    let textSize: CGFloat
+    
+    init(
+        _ id: String,
+        callback: @escaping (String)->(),
+        selectedID: String,
+        size: CGFloat = 20,
+        color: Color = Color.primary,
+        textSize: CGFloat = 14
+    ) {
+        self.id = id
+        self.size = size
+        self.color = color
+        self.textSize = textSize
+        self.selectedID = selectedID
+        self.callback = callback
+    }
+    
+    var body: some View {
+        Button(action:{
+            self.callback(self.id)
+        }) {
+            HStack(alignment: .center, spacing: 10) {
+                Image(systemName: self.selectedID == self.id ? "largecircle.fill.circle" : "circle")
+                    .renderingMode(.original)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: self.size, height: self.size)
+                    .modifier(ColorInvert())
+                Text(id)
+                    .font(Font.system(size: textSize))
+                Spacer()
+            }.foregroundColor(self.color)
+        }
+        .foregroundColor(self.color)
+    }
+}
+
+struct RadioButton_Previews: PreviewProvider {
+    static var previews: some View {
+        RadioButton("1", callback: { text in
+            
+        }, selectedID: "2")
+    }
+}
