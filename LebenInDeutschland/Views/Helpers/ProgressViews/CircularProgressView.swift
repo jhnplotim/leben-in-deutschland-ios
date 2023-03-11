5811c282142a6924294d31a9c2a74780c123e02b
John Paul Otim <jhnplotim@gmail.com>
2023-03-11 08:29:13 +0100

    feat: CircularProgressView initial
    - CircularProgressView initial version
    - moved PercentageView into ProgressViews group

---
+++ LebenInDeutschland/Views/Helpers/ProgressViews/CircularProgressView.swift
@@ -0,0 +1,56 @@
+//
+//  CircularProgressView.swift
+//  LebenInDeutschland
+//
+//  Created by John Paul Otim on 11.03.23.
+//
+
+import SwiftUI
+
+struct CircularProgressView: View {
+    
+    let progress: Double
+    
+    var lineWidth: CGFloat = 30
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .stroke(
+                    Color.green.opacity(0.5),
+                    lineWidth: lineWidth
+            )
+            
+            Circle()
+                .trim(from: 0, to: progress)
+                .stroke(
+                    Color.green,
+                    style: StrokeStyle(
+                        lineWidth: lineWidth,
+                        lineCap: .round
+                    )
+                )
+                .rotationEffect(.degrees(-90))
+                .animation(.easeOut, value: progress)
+            
+        }
+    }
+}
+
+struct CircularProgressView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            CircularProgressView(progress: 0.25, lineWidth: 5)
+                .frame(width: 50, height: 50)
+            
+            CircularProgressView(progress: 0.5)
+                .frame(width: 200, height: 200)
+            
+            CircularProgressView(progress: 0.8)
+                .frame(width: 200, height: 200)
+            
+            CircularProgressView(progress: 1)
+                .frame(width: 200, height: 200)
+        }
+    }
+}
