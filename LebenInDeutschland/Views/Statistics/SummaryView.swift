cf6ccf06086598be4e0658f1cf762db5d01e5c55
John Paul Otim <jhnplotim@gmail.com>
2023-03-15 10:30:45 +0100

    feat: very simple summary view implemented
    - very simple summary view implemented

--- LebenInDeutschland/Views/Statistics/SummaryView.swift
+++ LebenInDeutschland/Views/Statistics/SummaryView.swift
@@ -8,13 +8,35 @@
 import SwiftUI
 
 struct SummaryView: View {
+    @EnvironmentObject var assessmentData: AssessmentManager
+    
     var body: some View {
-        Text("Summary")
+        NavigationView {
+            if let examCount = assessmentData.examsDone.count, let answeredCount = assessmentData.chosenAnswers.count, examCount > 0 || answeredCount > 0 {
+                ScrollView {
+                    VStack(spacing: 20) {
+                        Text("\(examCount) exam(s) attempt so far")
+                            .font(.body)
+                        Text("\(answeredCount) non-unique question(s) attempted  so far")
+                            .font(.body)
+                    }
+                }
+                .navigationTitle("Summary")
+                .navigationBarTitleDisplayMode(.inline)
+                
+            } else {
+                Text("No question / exam attempts so far")
+                    .navigationTitle("Summary")
+                    .navigationBarTitleDisplayMode(.large)
+            }
+            
+        }
     }
 }
 
 struct SummaryView_Previews: PreviewProvider {
     static var previews: some View {
         SummaryView()
+            .environmentObject(AssessmentManager())
     }
 }
