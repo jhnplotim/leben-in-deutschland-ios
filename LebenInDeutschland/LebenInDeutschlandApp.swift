285d24a41ee1012913f3b613115c00e179311f4c
John Paul Otim <jhnplotim@gmail.com>
2023-05-30 22:16:19 +0200

    feat: added firebase analytics
    - added firebase analytics without the GoogleService-Info.plist file. To be added by whoever is interested in using it.

--- LebenInDeutschland/LebenInDeutschlandApp.swift
+++ LebenInDeutschland/LebenInDeutschlandApp.swift
@@ -7,9 +7,22 @@
 
 import SwiftUI
 import Swinject
+import FirebaseCore
+
+// TODO: Initialise
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
 struct LebenInDeutschlandApp: App {
+    
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
 
     @StateObject private var launchScreenStateMgr = LaunchScreenStateManager()
     
