3d33d34b81b595556b242e5041b5f49eb66b9576
John Paul Otim <jhnplotim@gmail.com>
2023-05-31 23:09:41 +0200

    feat: added Google AdMob CocoaPods lib
    - Added Google AdMob CocoaPods lib & initialised it

--- LebenInDeutschland/LebenInDeutschlandApp.swift
+++ LebenInDeutschland/LebenInDeutschlandApp.swift
@@ -7,16 +7,6 @@
 
 import SwiftUI
 import Swinject
-import FirebaseCore
-
-// TODO: Initialise
-class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-    return true
-  }
-}
 
 @main
 struct LebenInDeutschlandApp: App {
